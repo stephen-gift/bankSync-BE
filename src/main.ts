d29914da3ffe40e10ee37fc6251652b36678e361
Stephen <79639335+stephen-gift@users.noreply.github.com>
2026-01-09 15:38:22 +0100

    Add Swagger integration and update dependencies

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,24 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('BankSync API')
+    .setDescription('BankSync Backend API documentation')
+    .setVersion('1.0')
+    .addTag('banksync')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api/docs', app, document);
+
   await app.listen(process.env.PORT ?? 3000);
+  console.log(`Server is running on port ${process.env.PORT ?? 3000}`);
+  console.log(
+    `Swagger is running on port ${process.env.PORT ?? 3000}/api/docs`,
+  );
 }
+// eslint-disable-next-line @typescript-eslint/no-floating-promises
 bootstrap();
