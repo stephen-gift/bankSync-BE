cc598793dbf2d2d9e6cb55218f165cb4b1df91f0
Stephen <79639335+stephen-gift@users.noreply.github.com>
2026-01-16 13:41:13 +0100

    feat: add user module with entity and service
    - Create User entity with email, password, firstName, lastName
    - Implement UsersService with create, findByEmail, findById methods
    - Add UsersModule with TypeORM integration
    - Export UsersService for use in other modules

---
+++ src/users/users.service.ts
@@ -0,0 +1,44 @@
+import { Injectable, ConflictException } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { User } from './entities/user.entity';
+import * as bcrypt from 'bcrypt';
+
+@Injectable()
+export class UsersService {
+  constructor(
+    @InjectRepository(User)
+    private usersRepository: Repository<User>,
+  ) {}
+
+  async create(
+    email: string,
+    password: string,
+    firstName?: string,
+    lastName?: string,
+  ): Promise<User> {
+    const existingUser = await this.findByEmail(email);
+    if (existingUser) {
+      throw new ConflictException('Email already exists');
+    }
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    const user = this.usersRepository.create({
+      email,
+      password: hashedPassword,
+      firstName,
+      lastName,
+    });
+
+    return this.usersRepository.save(user);
+  }
+
+  async findByEmail(email: string): Promise<User | null> {
+    return this.usersRepository.findOne({ where: { email } });
+  }
+
+  async findById(id: string): Promise<User | null> {
+    return this.usersRepository.findOne({ where: { id } });
+  }
+}
